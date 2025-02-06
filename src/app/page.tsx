0c8089046cc53aa5dbc32dce8f7bbec31852ba5f
aazda <jackismebro@gmail.com>
2025-02-07 05:52:10 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,6 @@ export default function Home() {
             client={client}
             connectButton={{
              label: "Connect",
-             className: "bg-[#98ff99] hover:bg-[#98ff99]/80 width: 10em;  height: 2em;",
             }}
           />  
         </div>
