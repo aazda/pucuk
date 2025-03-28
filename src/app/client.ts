b5c221ec2971e0d0b3733af81d08f33b3c9f41a4
aazda <jackismebro@gmail.com>
2025-03-28 12:11:26 +0700

    update

--- src/app/client.ts
+++ src/app/client.ts
@@ -2,5 +2,5 @@
 import { createThirdwebClient } from "thirdweb";
 
 export const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
+  clientId: process.env.NEXT_PUBLIC_TW_CLIENT_ID as string,
 });
